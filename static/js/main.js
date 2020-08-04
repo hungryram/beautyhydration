50a6cd9ba59a1da142e97391180224a1194fa09d
Ram Dettmer <ramdettmer751@gmail.com>
2020-08-04 12:23:40 -0700

    updated cart

--- static/js/main.js
+++ static/js/main.js
@@ -11,11 +11,57 @@ $('.home-slide').slick({
   autoplay: true,
   speed: 1000,
   autoplaySpeed: 5000,
+  dots: true,
 });
 
 
 // LIGHTBOX
 $('.slider-for').slickLightbox();
+
+// PRODUCTS
+$('.product-slide').slick({
+  infinite: true,
+  dots: true,
+  adaptiveHeight: true,
+  speed: 1000,
+  });
+
+// OTHER PRODUCTS SCROLL
+$('.other-products').slick({
+  dots: true,
+  infinite: true,
+  speed: 1000,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
 
 $('.responsive-slide').slick({
   dots: true,
